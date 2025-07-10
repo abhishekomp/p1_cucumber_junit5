2b0540e1c64b4cc7efb339220331fb30fa8e15b3
abhishekomp <abhishek2283@hotmail.com>
2025-07-10 18:48:30 +0530

    Added Cucumber reporting and works fine

--- src/test/java/org/example/runner/CucumberRunnerTest.java
+++ src/test/java/org/example/runner/CucumberRunnerTest.java
@@ -6,10 +6,14 @@ import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.example.stepdefs")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:target/cucumber-report.html")
 public class CucumberRunnerTest {
 }
+
+/*@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")*/
